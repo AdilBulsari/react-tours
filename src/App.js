2e15d0c64ddb4019a62ac6dadb85d8bd6fd1a4a0
AdilBulsari <adilbulsari@gmail.com>
2022-10-05 23:03:31 +0100

    added tours list

--- src/App.js
+++ src/App.js
@@ -4,8 +4,41 @@ import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
 const url = "https://course-api.com/react-tours-project";
+
 function App() {
-  return <h2>Tours Project Setup</h2>;
+  const [loading, setLoading] = useState(false);
+  const [tourData, setTourData] = useState([]);
+
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTourData(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <Tours tours={tourData} />
+    </main>
+  );
 }
 
 export default App;
