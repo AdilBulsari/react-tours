eb3b0c3b1da4b3cad00eb17aa2c061652032c1de
AdilBulsari <adilbulsari@gmail.com>
2022-10-05 23:19:31 +0100

    added show para content toggle

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
 
 const Tour = ({ id, image, info, price, name }) => {
+  const [readMore, setReadMore] = useState(false);
+
+  const readMoreInfo = () => {
+    setReadMore(!readMore);
+  };
   return (
     <article className="single-tour">
       <img src={image} alt={name} />
@@ -9,7 +14,13 @@ const Tour = ({ id, image, info, price, name }) => {
           <h4>{name}</h4>
           <h4 className="tour-price">£{price}</h4>
         </div>
-        <p>{info}</p>
+        <p>
+          {readMore ? info : `${info.substring(0, 200)}...`}
+          <button onClick={readMoreInfo} className="single-tour">
+            {!readMore ? "show more..." : "show less..."}
+          </button>
+        </p>
+
         <button className="delete-btn">Not Interested</button>
       </footer>
     </article>
